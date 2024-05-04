5c50d126d18462d909c58dcae12d285e7e867da8
Daniel jenkins <djenkinsjg0617@gmail.com>
2024-05-04 13:50:02 -0500

    file path should be treated as a string

--- testing/file_test.py
+++ testing/file_test.py
@@ -1,6 +1,7 @@
 import csv
 
-f_path = input("Enter the path to a .txt or .csv file containing hashes :: ")
+f_path = str(
+    input("Enter the path to a .txt or .csv file containing hashes :: "))
 
 file = open(f_path, 'r')
 print(file.read())
