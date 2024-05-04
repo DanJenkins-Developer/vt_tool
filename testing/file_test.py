03bfb8d8bc0c8b78ac95ad29570ac19674512cad
Daniel jenkins <djenkinsjg0617@gmail.com>
2024-05-04 13:58:10 -0500

    test hashes

--- testing/file_test.py
+++ testing/file_test.py
@@ -1,7 +1,11 @@
 import csv
 
-f_path = str(
-    input("Enter the path to a .txt or .csv file containing hashes :: "))
+# f_path = str(
+#     input("Enter the path to a .txt or .csv file containing hashes :: "))
+
+test_f_path = "C:\\Users\\skydr\\Documents\\Development\\vt_tool\\testing\\test.txt"
+f_path = test_f_path
+
 
 file = open(f_path, 'r')
 print(file.read())
